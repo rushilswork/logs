2a0d81a372eda7af8f104cd2bcea3ac64973b31e
Rushil Pillamari <pillamari.r@cubeware.de>
2025-11-06 02:30:13 +0530

    updated intuition

--- bitwisepowerset.cpp
+++ bitwisepowerset.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 typedef long long int ll;
 
+
+// logic behind using bitmasking is:
+// we have 3 elements ( lets suppose : 1 2 3) to find all subsets ( powerset ) we try out all combinations choosing and not choosing elements to makeout all possible combinations
+// mathematically for powerset , the total count of possible subsets is 2 ^ n here it is 2 ^ 3 (or) (1 << n) here ( 1 << 3 )
+// similarly suppose we have 3 bits, number of possible combinations that we can make out of 0 and 1 ( binary numbers ) are 2 ^ 3
+// choosing and not choosing ( 2 choices ) are mapped to 1 and 0, and the total number of combinations of these 3 bits mapped to total combinations of 3 numbers which yields subsets
+// considering the set bits in the possible combinations tells us what number to be choosen for the instance of subset combination / binary combination
+// 1 (set bit) - choose
+// 0 (unset bit) - dont choose
+
 void TCSOLVE(){
     ll n;
     cin >> n;
