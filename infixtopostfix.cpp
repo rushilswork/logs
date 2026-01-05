92ec915659aee60e8fc1f9f69fcba6a34362bff2
Rushil Pillamari <pillamari.r@cubeware.de>
2026-01-06 00:25:10 +0530

    infix to postfix refined logic

--- infixtopostfix.cpp
+++ infixtopostfix.cpp
@@ -3,61 +3,56 @@ using namespace std;
 
 typedef long long int ll;
 
-//a+b*(c^d-c)
-void infixtopostfix(string &s){
-    map<char,int> m = {
-        {'^',3},
-        {'*',2},
-        {'/',2},
-        {'+',1},
-        {'-',1},
-    };
-
-    stack<char> stc;
-    string letters="abcdefghijklmnopqrstuvwxyz";
-    string ans="";
-    for(int i =0 ;i<s.length();i++){
-        // operands
-        if(letters.find(s[i])!=string::npos){
-            ans+=s[i];
-        }
-        // operators
-        else{
-            if(stc.empty()){
-                stc.push(s[i]);
-            }
-            else{
-                // in stack always the high priority symbol should be present, so if theres any less priority one is about to be added to stack check and remove all the highpriorty ones and add them to ans
-                while(!stc.empty() && s[i] != '(' && s[i] != ')' && m[s[i]] <= m[stc.top()]){
-                    ans+=stc.top();
-                    stc.pop();
-                }
-                
-                // when encountered ')' make sure to remove all the ones in the stack until you encounter 
-                if(s[i] == ')'){
-                    while(!stc.empty() && stc.top() !='('){
-                        ans+=stc.top();
-                        stc.pop();
-                    }
-                    // safety check
-                    if(stc.top() == '('){
-                        stc.pop();
-                    }
-                    continue;
-                }
-                // after removing add the current one
-                // in the same line the case where high priority ones will be handled
-                stc.push(s[i]);
-                // in the same line the case where '(' will be handled  
-            }
-        }
-        // stack<char> temp = stc;
-        // while(!temp.empty()){
-        //     cout << temp.top() << " ";
-        //     temp.pop();
-        // }
-        
+int prec(char c){
+    if(c == '^'){
+        return 3; 
     }
+    else if(c == '*' || c == '/'){
+        return 2;
+    }
+    else if(c == '+' || c == '-'){
+        return 1;
+    }
+    else{
+        return -1;
+    }
+}
+
+void infixtopostfix(string &s){
+    string ans = "";
+    stack<int> stc;
+    for(int i = 0;i<s.length();i++){
+        char c = s[i];
+        // for operands
+        if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')){
+            ans+=c;
+        }
+        else if(c == '('){
+            stc.push(c);
+        }
+        else if(c == ')'){
+            while(!stc.empty() && stc.top() != '('){
+                ans+=stc.top();
+                stc.pop();
+            }
+
+            // safety check
+
+            if(stc.top() == '('){
+                stc.pop();
+            }
+        }
+        // for operators
+        else{
+            while(!stc.empty() && prec(c) <= prec(stc.top())){
+                ans+=stc.top();
+                stc.pop();
+            }
+            stc.push(c);
+        }
+    }
+
+    // the operators remained in the stack need to be added back into the ans
     while(!stc.empty()){
         ans+=stc.top();
         stc.pop();
@@ -65,13 +60,9 @@ void infixtopostfix(string &s){
     s = ans;
 }
 
-//a*(b+c)/d
-// stack - *(+
-// ans  - abc+
 void TCSOLVE(){
     string s;
     cin >> s;
-
     infixtopostfix(s);
     cout << s << endl;
 }
