53ba2c290118f7071acbe64642b88eada8a5be8a
Rushil Pillamari <pillamari.r@cubeware.de>
2026-01-06 00:40:33 +0530

    its is just reverse of infix first and peform the infxi to postfix on the revered string and then do the reverse of the string, but one important thing to be noted in the conversion of infix to prefix is that the equal priority operator will not be popped

---
+++ infixtoprefix.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+int prec(char c){
+    if(c == '^'){
+        return 3; 
+    }
+    else if(c == '*' || c == '/'){
+        return 2;
+    }
+    else if(c == '+' || c == '-'){
+        return 1;
+    }
+    else{
+        return -1;
+    }
+}
+
+void infixtopostfix(string &s){
+    string ans = "";
+    stack<int> stc;
+    for(int i = 0;i<s.length();i++){
+        char c = s[i];
+        // for operands
+        if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')){
+            ans+=c;
+        }
+        else if(c == '('){
+            stc.push(c);
+        }
+        else if(c == ')'){
+            while(!stc.empty() && stc.top() != '('){
+                ans+=stc.top();
+                stc.pop();
+            }
+
+            // safety check
+
+            if(stc.top() == '('){
+                stc.pop();
+            }
+        }
+        // for operators
+        else{
+            // in infix to prefix we dont't pop equal priority operators
+            while(!stc.empty() && prec(c) < prec(stc.top())){
+                ans+=stc.top();
+                stc.pop();
+            }
+            stc.push(c);
+        }
+    }
+
+    // the operators remained in the stack need to be added back into the ans
+    while(!stc.empty()){
+        ans+=stc.top();
+        stc.pop();
+    }
+    s = ans;
+}
+
+
+void infixtoprefix(string& s){
+    int idx1 = -1;
+    int idx2 = -1;
+    idx1 = s.find('(');
+    idx2 = s.find(')');
+
+    if(idx1!=-1 && idx2!=-1){
+        s.replace(idx1,1,1,')');
+        s.replace(idx2,1,1,'(');
+    }
+    reverse(s.begin(),s.end());
+
+    infixtopostfix(s);
+    reverse(s.begin(),s.end());
+}
+void TCSOLVE(){
+    string s;
+    cin >> s;
+    infixtoprefix(s);
+    cout << s << endl;
+}
+int main(){
+    ll n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    n = 1;
+    while(n--){
+        TCSOLVE();
+    }
+}
