b03a79fdcc9632943da4126acea2379f792c0025
Rushil Pillamari <pillamari.r@cubeware.de>
2026-01-06 19:57:37 +0530

    postfix to infix implementation

---
+++ postfixtoinfix.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+
+void postfixtoinfix(string &s){
+    string ans="";
+    stack<string> stc;
+    for(int i =0 ;i<s.length();i++){
+        char c = s[i];
+        if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')){
+            stc.push(string(1,c));
+        }
+        // operator
+        else{
+            if(stc.size() >= 2){
+                string b = stc.top();
+                stc.pop();
+                string a = stc.top();
+                stc.pop();
+                string temp = '(' + a + c + b + ')';
+                stc.push(temp);
+            }
+        }
+    }
+
+    stringstream ss;
+    ss << stc.top();
+    ss >> ans;
+    cout << ans << endl;
+
+}
+void TCSOLVE(){
+    string s;
+    cin >> s;
+    postfixtoinfix(s);
+}
+int main(){
+    ll n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    n = 1;
+    while(n--){
+        TCSOLVE();
+    }
+}
