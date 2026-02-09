4457dcee771a294fd9c9be1e386c83a80cc6fd6b
Rushil Pillamari <pillamari.r@cubeware.de>
2026-02-09 20:57:31 +0530

    implementation of nicesubarrays with k odd numbers

---
+++ nicesubarrays.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+int utilityfunction(vector<int>&v,int k){
+    if(k == -1){
+        return 0;
+    }
+    int n = v.size();
+    int l = 0;
+    int r = 0;
+    int sum = 0;
+    int cnt = 0;
+    while(r < n){
+        sum+=v[r];
+        while(sum > k){
+            sum-=v[l];
+            l++;
+        }
+        // while the sum is sum <= k, calculate the subarrays, basically we are building the count internally we are applying divide and conquer logic not techinically but logically, 
+        /*
+            1 0 0 1 1 0 
+            in the subarray
+            1 -> 1 
+            1 0 -> total of 3 => 1 , 1 0, 0
+            1 0 0 -> total of 6 => 1, 1 0, 1 0 0 , 0 , 0 0, 0 ( but in this few of them we already calculated in the previous step )
+            so when you reach at a particular index, check how many subarrays does this index add up to previous subarrays
+        */
+        cnt+=r-l+1;
+        r++;
+    }
+    return cnt;
+}
+
+void TCSOLVE(){
+    ll n;cin >> n;
+    vector<int> v;
+    for(int i =0 ;i<n;i++){
+        ll x;
+        cin >> x;
+        v.push_back(x);
+    }
+    int k;
+    cin >> k;
+
+    cout << utilityfunction(v,k) - utilityfunction(v,k-1) << endl;
+}
+int main(){
+    ll n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    n = 1;
+    while(n--){
+        TCSOLVE();
+    }
+}
