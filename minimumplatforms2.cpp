f0af593ca2610ed9af83321ab6f6edd367df0151
Rushil Pillamari <pillamari.rushil@insightsoftware.com>
2026-04-02 09:34:31 +0530

    optimal approach

---
+++ minimumplatforms2.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+
+void TCSOLVE(){
+    ll n;
+    cin >> n;
+    vector<pair<int,char>> v;
+    for(int i = 0;i<n;i++){
+        ll x,y;
+        cin >> x >> y;
+        v.push_back({x,'A'});
+        v.push_back({y,'D'});
+    }       
+
+
+    sort(v.begin(),v.end());
+
+    int maxcnt = INT_MIN;
+    int cnt = 0;
+    for(auto x: v){
+        if(x.second == 'A'){
+            cnt++;
+        }
+        else{
+            cnt--;
+        }
+
+        maxcnt = max(cnt,maxcnt);
+    }
+
+    cout << maxcnt << endl;
+}
+int main(){
+    ll n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    n = 1;
+    while(n--){
+        TCSOLVE();
+    }
+}
